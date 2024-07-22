217218d4a413604ee8127e834202306cc179265a
Devam Barbahya <barbhaya.devam@gmail.com>
2024-07-23 02:54:10 +0530

    Authentication - Clerk

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,14 +7,14 @@ const Header = ({ children }: HeaderProps) => {
     <div className="header">
       <Link href="/" className="md:flex-1">
         <Image
-          src="/assets/icons.logo.svg"
+          src="/assets/icons/logo.svg"
           alt="Logo with name"
           width={120}
           height={32}
           className="hidden md:block"
         />
         <Image
-          src="/assets/icons.logo-icon.svg"
+          src="/assets/icons/logo-icon.svg"
           alt="Logo without name"
           width={32}
           height={32}
